9068e1f6fa1688462c7327fa3a658765ef7bd409
Aatish Rana <aatishrana8@gmail.com>
2017-12-06 12:44:42 +0530

    basic setup

---
+++ vendor/generator/generator.go
@@ -0,0 +1,65 @@
+package generator
+
+type Entity struct {
+	ID          int `sql:"AUTO_INCREMENT"`
+	Name        string `sql:"type:varchar(30)"  gorm:"column:alias_name"`
+	DisplayName string `sql:"type:varchar(30)"`
+	Columns     []Column `gorm:"ForeignKey:entity_id;AssociationForeignKey:id"` // one to many, has many columns
+}
+
+type ColumnType struct {
+	ID      int    `sql:"AUTO_INCREMENT"`
+	Type    string `sql:"type:varchar(30)"`
+	Columns []Column `gorm:"ForeignKey:type_id;AssociationForeignKey:id"` //one to many, has many columns
+}
+
+type Column struct {
+	ID         int `sql:"AUTO_INCREMENT"`
+	Name       string `sql:"type:varchar(30)"`
+	Size       uint `sql:"type:int(30)"`
+	TypeID     uint `sql:"type:int(30)"`
+	EntityID   uint `sql:"type:int(100)"`
+	ColumnType ColumnType `gorm:"ForeignKey:TypeID"` //belong to (for reverse access)
+}
+
+type RelationType struct {
+	ID   int `sql:"AUTO_INCREMENT"`
+	Name string `sql:"type:varchar(30)"`
+}
+
+type Relation struct {
+	ID                int `sql:"AUTO_INCREMENT"`
+	ParentEntityID    int `sql:"type:int(100)"`
+	ParentEntityColID int `sql:"type:int(100)"`
+	ChildEntityID     int `sql:"type:int(100)"`
+	ChildEntityColID  int `sql:"type:int(100)"`
+	InterEntityID     int `sql:"type:int(100)"`
+	RelationTypeID    int `sql:"type:int(10)"`
+
+	ParentEntity Entity `gorm:"ForeignKey:ParentEntityID"`       //belong to
+	ChildEntity  Entity `gorm:"ForeignKey:ChildEntityID"`        //belong to
+	InterEntity  Entity `gorm:"ForeignKey:InterEntityID"`        //belong to
+	ParentColumn Column `gorm:"ForeignKey:ParentEntityColID"`    //belong to
+	ChildColumn  Column `gorm:"ForeignKey:ChildEntityColID"`     //belong to
+	RelationType RelationType `gorm:"ForeignKey:RelationTypeID"` //belong to
+}
+
+func (Entity) TableName() string {
+	return "c_entity"
+}
+
+func (ColumnType) TableName() string {
+	return "c_column_type"
+}
+
+func (Column) TableName() string {
+	return "c_column"
+}
+
+func (RelationType) TableName() string {
+	return "c_relation_type"
+}
+
+func (Relation) TableName() string {
+	return "c_relation"
+}
